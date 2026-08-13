ae4713eba5d0050151993b4597a922daf3ee42c3
DevWebEmmanuel <devwebemmanuel@gmail.com>
2026-08-13 19:29:49 +0100

    Retrieving the user's choice
    Creating a function that returns the user's valid choice.

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function getComputerChoice() {
     let table = ["Rock", "Paper", "Scissors"];
     return table[Math.floor(Math.random() * table.length)]
 }
+
+// Secondly: Creating the logic to obtain the human's choice 
+
+function getHumanChoice() {
+    let message = prompt("Choose between 'Rock', 'Paper' and 'Scissors': ");
+    return message
+}
