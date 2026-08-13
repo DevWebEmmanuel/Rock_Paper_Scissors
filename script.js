b39d74e94254a11558b002cc456b7ce6a8844de0
DevWebEmmanuel <devwebemmanuel@gmail.com>
2026-08-13 20:21:55 +0100

    Results of a single game played
    Create a function that displays the results of a single game in the console, indicates the winner, and updates their winning score.

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+// Fourthly: logic to play a single round
+
+function playRound(humanChoice, computerChoice) {
+    let human = humanChoice.toLowerCase();
+    let computer = computerChoice.toLowerCase();
+
+    if ((human === "rock" && computer === "paper") || 
+    (human === "scissors" && computer === "rock") || 
+    (human === "paper" && computer === "scissors")) {
+        console.log("Vous avez perdu la partie !");
+        computerScore++;
+    } else if ( (computer === "rock" && human === "paper") || 
+    (computer === "scissors" && human === "rock") || 
+    (computer === "paper" && human === "scissors") ) {
+        console.log("Vous avez gagné la partie !");
+        humanScore++;
+    } else {
+        console.log("La partie est nulle.\nPas de gagant");
+    }
+}
